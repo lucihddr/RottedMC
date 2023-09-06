2e48b5071fcf874d142af6409ff11da682facfb4
lucihddr <sparki.fn@gmail.com>
2023-09-06 21:21:51 +0300

    ok

--- src/main/java/net/workswave/extra/ZombieDies.java
+++ src/main/java/net/workswave/extra/ZombieDies.java
@@ -1,37 +1,31 @@
+
 package net.workswave.extra;
 
 
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.monster.Zombie;
-import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.workswave.item.ModItems;
+
 
 @Mod.EventBusSubscriber
-public class DaProcedure {
-    @SubscribeEvent
-    public static void onEntityDeath(LivingDeathEvent event) {
-        if (event != null && event.getEntity() != null) {
-            execute(event, event.getEntity().level(), event.getEntity().getX(), event.getEntity().getY(), event.getEntity().getZ(), event.getEntity());
+public class ZombieDies {
+    public static Zombie zombie;
+    final ItemStack itemStack = new ItemStack(ModItems.ROTTEN.get());
+    double x = zombie.getX();
+    double y = zombie.getY();
+    double z = zombie.getZ();
+    public ZombieDies(Zombie zombie){
+        this.zombie = zombie;
+    }
+
+    public static void OnEntityDeath(LivingDeathEvent event) {
+        if (event != null && event.getEntity() == zombie && event.getEntity() != null && zombie.getRandom().nextInt(400) == 0) {
+
         }
     }
 
-    public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
-        execute(null, world, x, y, z, entity);
-    }
 
-    private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
-        if (entity == null)
-            return;
-        if (entity instanceof Zombie) {
-            if (world instanceof ServerLevel _level) {
-                ItemEntity = new ItemEntity(_level, x, y, z, new ItemStack(Items.ROTTEN_FLESH));
-                entitytospaw.setPickUpDelay(10);
-                _level.addFreshEntity(entityToSpawn);
-            }
-        }
-    }
+
 }
